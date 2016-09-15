c53d13ff40761f559bdcd2da15e6540fb82eb0ea
Razevich <Razevich.c@gmail.com>
2016-09-14 20:05:53 -0700

    new commit for jamie

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,13 @@ class Project < ActiveRecord::Base
     has_many :marketings
     has_many :independednt_films
 
+    def create_object(object)
+      if object == "tv"
+        self.tv.new(tv_params)
+      elsif object == "film"
+        self.tv.net(film_params)
+      elsif object == "independednt_films"
+        self.tv.net(independednt_films_params)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :staffs
+  #
+  # resources :staffs
   get 'home/index'
   root "home#index"
   get 'home/services'
 
   # resources :services
-  resources :contacts
-  resources :clients
-  resources :films
+  # resources :contacts
+  # resources :clients
+  # resources :films
   # resources :staff
   # resources :projects
 
@@ -18,32 +18,32 @@ Rails.application.routes.draw do
 
   get "projects", to: "projects#index"
 
-  # get "staff", to: "staff#index"
-  # post "staff", to: "staff#create"
-  # get "staff/new", to: "staff#new", as: "new_staff"
-  # get "staff/:name/edit", to: "staff#edit", as: "edit_staff"
-  # put "staff/:name", to: "staff#update", as: "update_staff"
-  # delete "staff/:name", to: "staff#delete"
+  get "staff", to: "staff#index"
+  post "staff", to: "staff#create"
+  get "staff/new", to: "staff#new", as: "new_staff"
+  get "staff/:name/edit", to: "staff#edit", as: "edit_staff"
+  put "staff/:name", to: "staff#update", as: "update_staff"
+  delete "staff/:name", to: "staff#delete"
 
-  # get "clients", to: "clients#index"
-  # post "clients", to: "clients#create"
-  # get "clients/new", to: "clients#new", as: "new_client"
-  # get "clients/:name/edit", to: "clients#edit"
-  # put "clients/:name", to: "clients#update", as: "update_client"
-  # delete "client/:name", to: "client#delete"
+  get "clients", to: "clients#index"
+  post "clients", to: "clients#create"
+  get "clients/new", to: "clients#new", as: "new_client"
+  get "clients/:name/edit", to: "clients#edit"
+  put "clients/:name", to: "clients#update", as: "update_client"
+  delete "client/:name", to: "client#delete"
 
   get "musicology_services", to: "musicology_services#index"
   get "musicology_services/edit", to: "musicology_services#edit"
   put "musicology_services", to: "musicology_services#update", as: "update_musicology_services"
   delete "musicology_services", to: "musicology_services"
 
-  # get "films", to: "films#index"
-  # get "films/:title",  to: "films#show"
-  # get "films/new", to: "films#new", as: "new_film"
-  # post "films", to: "films#create"
-  # get "films/:title/edit", to: "films#edit"
-  # put "films/:title", to: "films#update", as: "update_film"
-  # delete "films/:title", to: "films#delete"
+  get "films", to: "films#index"
+  get "films/:title",  to: "films#show"
+  get "films/new", to: "films#new", as: "new_film"
+  post "films", to: "films#create"
+  get "films/:title/edit", to: "films#edit"
+  put "films/:title", to: "films#update", as: "update_film"
+  delete "films/:title", to: "films#delete"
 
   get "independent_films", to: "independent_films#index"
   get "independent_films/:title",  to: "independent_films#show"
@@ -53,13 +53,13 @@ Rails.application.routes.draw do
   put "independent_films/:title", to: "independent_films#update", as: "update_indie_film"
   delete "independent_films/:title", to: "independent_films#delete"
 
-  # get "tv", to: "tv#index"
-  # get "tv/:title",  to: "tv#show"
-  # get "tv/new", to: "tv#new", as: "new_tv"
-  # post "tv", to: "tv#create"
-  # get "tv/:title/edit", to: "tv#edit"
-  # put "tv/:title", to: "tv#update", as: "update_tv"
-  # delete "tv/:title", to: "tv#delete"
+  get "tv", to: "tv#index"
+  get "tv/:title",  to: "tv#show"
+  get "tv/new", to: "tv#new", as: "new_tv"
+  post "tv", to: "tv#create"
+  get "tv/:title/edit", to: "tv#edit"
+  put "tv/:title", to: "tv#update", as: "update_tv"
+  delete "tv/:title", to: "tv#delete"
 
   get "marketing", to: "marketing#index"
   get "marketing/:title",  to: "marketing#show"
